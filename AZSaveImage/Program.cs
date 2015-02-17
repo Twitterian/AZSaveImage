18d4b0569e5fe878a85c5600913e5b0a5290af48
Tokino <tjddlf2645@gmail.com>
2015-02-18 05:05:54 +0900

    마지막 위치 저장 기능 추가

--- AZSaveImage/Program.cs
+++ AZSaveImage/Program.cs
@@ -11,6 +11,8 @@ namespace AZSaveImage
 {
     static class Program
     {
+        const string path = "Usagination_Addon";
+
         [STAThread]
         static void Main(string[] args)
         {
@@ -18,14 +20,42 @@ namespace AZSaveImage
             // 파싱을 위해 정규식을 만듭니다.
             var regx = new Regex(@"media_url:http:\/\/[^\/]+\/media\/[^.]+.[a-z]+");
 
-            if (args.Length != 0)
-            {
-                // 이미지 저장 경로를 지정할 다이얼로그입니다.
-                var savefile = new SaveFileDialog();
-                savefile.InitialDirectory = Directory.GetCurrentDirectory();
-                savefile.Title = "이미지 저장";
-                savefile.Filter = "이미지 파일|*.*";
+            // 이미지 저장 경로를 지정할 다이얼로그입니다.
+            var savefile = new SaveFileDialog();
+            savefile.RestoreDirectory = true;
+            savefile.Title = "이미지 저장";
+            savefile.Filter = "이미지 파일|*.*";
 
+            // 설정 파일을 저장할 경로입니다. 없다면 만들어줘야죠!
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path).Attributes = FileAttributes.Directory | FileAttributes.Hidden;
+                savefile.InitialDirectory = Environment.CurrentDirectory;
+            }
+            else if (!File.Exists(path + "/AzImageSave.dat"))
+            {
+                savefile.InitialDirectory = Environment.CurrentDirectory;
+            }
+            else // 설정을 읽어와요
+            {
+                try
+                {
+                    using (System.IO.Stream ReadStream = new FileStream(path + "/AzImageSave.dat", FileMode.Open))
+                    {
+                        var Reader = new StreamReader(ReadStream);
+                        savefile.InitialDirectory = Reader.ReadLine();
+                        Reader.Close();
+                    }
+                }
+                catch
+                {
+                    savefile.InitialDirectory = Environment.CurrentDirectory;
+                }
+            }
+
+
+            if (args.Length > 0)
+            {
                 // 전달받은 트윗 객체에 미디어가 4개 미만이어도 항상 링크는 4개 돌려받습니다.
                 // 의도는 알 수 없지만, 같은 이미지를 여러번 저장하는 것은 무의미하기 때문에 여기선 파일 이름이 같다면 다이얼로그를 띄우지 않도록 합니다.
                 var list = new List<string>();
@@ -34,7 +64,7 @@ namespace AZSaveImage
 
                 while (match.Success)
                 {
-                    var url = match.Value.Replace("media_url:", ""); 
+                    var url = match.Value.Replace("media_url:", "");
                     var image = GetImageFromUrl(url); // url로부터 이미지 스트림을 얻어옵니다.
                     savefile.FileName = url.Replace("http://pbs.twimg.com/media/", ""); // url에서 끝 부분만 잘라내 파일 이름의 기본값으로 설정합니다.
                     if (list.Contains(savefile.FileName)) // 해당 값이 이미 리스트에 있다면 이 이미지에 대한 저장작업은 한번 이상 수행되었을 것임으로 다음 매치를 찾습니다.
@@ -43,13 +73,24 @@ namespace AZSaveImage
                         continue;
                     }
                     list.Add(savefile.FileName); // 이미지 파일 이름을 리스트에 추가합니다.
-                    if (savefile.ShowDialog() == DialogResult.OK) 
+                    if (savefile.ShowDialog() == DialogResult.OK)
                     {
                         image.Save(savefile.FileName); // 이미지 저장
                     }
+
+                    // 파일 이름으로 전체 경로를 얻어온다 -> 거기서 파일이름을 뺀다 = 현재 디렉터리!
+                    savefile.InitialDirectory = Path.GetDirectoryName(savefile.FileName).Replace(savefile.FileName, ""); 
+
                     match = match.NextMatch();
                 }
 
+                // 종료하기 전에 잊지말고 설정 저장하기
+                using (System.IO.Stream WriteStream = new FileStream(path + "/AzImageSave.dat", FileMode.Create))
+                {
+                    var Writer = new StreamWriter(WriteStream);
+                    Writer.WriteLine(savefile.InitialDirectory);
+                    Writer.Close();
+                }
             }
         }
 
--- SaveImage.js
+++ SaveImage.js
@@ -1,5 +1,8 @@
 // By - @Lolicon_sagi
 // Last Update : 2015-02-18
+
+// 이미지를 내려받고싶은 트윗을 선택하고 Ctrl + S
+
 function SaveImage(id) {
     var urls = TwitterService.call('statuses/lookup.json?id=20,' + id).replace(/\\/g, '');
     System.launchApplication('AZSaveImage.exe', urls, 1);

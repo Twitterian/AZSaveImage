240181e1dfb3784bc25d642be30e3c6373920cff
Tokino <tjddlf2645@gmail.com>
2015-02-18 16:11:00 +0900

    버그 수정
    가끔씩 오류나던거

--- SaveImage.js
+++ SaveImage.js
@@ -5,6 +5,6 @@
 
 function SaveImage(id) {
     var urls = TwitterService.call('statuses/lookup.json?id=20,' + id).replace(/\\/g, '');
-    System.launchApplication('AZSaveImage.exe', urls, 1);
+    System.launchApplication('AZSaveImage.exe', urls, 0);
 }
 System.addKeyBindingHandler('S'.charCodeAt(0), 2, SaveImage);
